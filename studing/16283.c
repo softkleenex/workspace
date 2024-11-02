8f2ebc2fa6f4957dd21ccb097adc293da743f779
softkleenex <softkleenex1217@gmail.com>
2024-11-02 20:07:14 +0900

    q

--- studing/16283.c
+++ studing/16283.c
@@ -38,42 +38,28 @@ int main(int argc, char* argv[])
 	}
 	//w = 양 * a + (n - 양)b, a != b 라면? w = 양 * a + (n-양)*b, w == 양 * a - 양 * b + n*b 
 	else
-		while (left <= right)
-		{
-		int mid = (left + right) / 2;
-		if (w == a * mid + b * (n - mid))
-		{
-			int ans1 = mid; int ans2 = n - mid;
-			if (ans1 > 0 && ans2 > 0)
-			{
-				ans1_post = ans1, ans2_post = ans2;
-				ans_count++;
-			}
-			if (left == right)
-			{
-				break;
-			}
-		}
-		if (w < a * mid + b * (n - mid))
-		{
+		for(int ans1 = 1; ans1 <= 1000; ans1++)
+        {
+            int cal = a*ans1 + b*(n - ans1);
+            ans1 = ans1; 
+            int ans2 = n - ans1;
+            if(cal == w && ((1 <= ans1 && ans1 <= 1000) && (1 <= ans2 && ans2 <= 1000)))
+            {
+                ans_count++;
+                ans1_post = ans1;
+                ans2_post = ans2;
+            }
+        }
 
-			left = mid + 1;
-		}
-		else if (w > a * mid + b * (n - mid))
-		{
-			right = mid - 1;
-		}
-	}
+if(ans_count == 1)
+{
+    printf("%d %d", ans1_post, ans2_post);
+}
+else
+{
+printf("%d", -1);
 
-
-
-	if (ans_count == 1)
-	{
-		printf("%d %d", ans1_post, ans2_post);
-	}
-	else {
-		printf("%d", -1);
-	}
+}
 
 	return 0;
 
